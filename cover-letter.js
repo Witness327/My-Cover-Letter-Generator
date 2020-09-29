d8fffd8a4fe8ce0a8da092c951504c27b7a3a67d
Jenn DeJesus <andresperez327@gmail.com>
2020-09-29 03:04:45 -0400

    Changed template format

--- cover-letter.js
+++ cover-letter.js
@@ -1,5 +1,5 @@
 
-alert("Welcome to Andres cover letter generator. Answer the following questions and receive your cover letter. Enjoy!");
+alert("Welcome to Andres' cover letter generator. Answer the following questions and receive your cover letter. Enjoy!");
 const role =prompt("What position are you applying for?");
 const companyName = prompt('What is the name of the company you are applying to?');
 
@@ -13,7 +13,8 @@ const coverLetter=
 Deerfield Beach, FL 33442 | (C) (561) 213-7707 | AndresPerez327@Gmail.com <br><br>
 
 ${month} ${day}, ${year}<br>
-RE: ${role}, Dear ${companyName},<br>
+RE: ${role} <br><br>
+Dear ${companyName}, <br>
 I read with enthusiasm your recent job opportunity as a ${role} and I am writing to express my deepest interest. When reviewing the position requirements and your organization’s website, I was excited to find that my qualifications and personal strengths align with your needs and mission.<br><br>
 
 What a thrill it would be to work for your team! I would love to speak with you regarding the position and how I could be a key player on your team. It would be an honor and a blessing to be a part of your company of leaders.<br><br>
