b18a52b49c25cbf45cb1c82df6d7a7e3f54a6143
Srijan0902 <72217861+Srijan0902@users.noreply.github.com>
2020-10-01 23:44:08 +0530

    Update cover-letter.js

--- cover-letter.js
+++ cover-letter.js
@@ -19,9 +19,9 @@ I read with enthusiasm your recent job opportunity as a ${role} and I am writing
 
 What a thrill it would be to work for your team! I would love to speak with you regarding the position and how I could be a key player on your team. It would be an honor and a blessing to be a part of your company of leaders.<br><br>
 
-I bring a comprehensive set of skills that I believe will be valuable to your company. In my time as a student and a freelance web developer, I honed my abilities in both frontend and backend development, providing a firm foundation for the ${role} position. I am excited to contribute my talents and strengths to your teams efforts.<br><br>
+I bring a comprehensive set of skills that I believe will be valuable for your company. In my time as a student and a freelance web developer, I honed my abilities in both frontend and backend development, providing a firm foundation for the ${role} position. I am excited to contribute my talents and strengths to your teams efforts.<br><br>
 
-Please review my enclosed resume for a more in-depth illustration of my work history and accomplishments. I would appreciate the opportunity to interview at your earliest convenience. I’m eager to discuss how my personality and background fit the ${role} role.<br><br>
+Please review my enclosed resume for a more in-depth illustration of my work history and accomplishments. I would appreciate the opportunity to interview at your earliest convenience. I am eager to discuss how my personality and background fit the ${role} role.<br><br>
 
 Please feel free to take a look at my project page below: <br>
 <a href="https://witness327.github.io/Project-Page/index.html" target="_blank"><strong>Witness327.GitHub.io/Project-Page</strong></a> <br><br>
@@ -30,3 +30,4 @@ Thank you for your time and consideration of my candidacy<br>
 Sincerely, Andres Perez </p>`
 
 document.querySelector('main').innerHTML = coverLetter;
+##you can elaborote more about yourself
