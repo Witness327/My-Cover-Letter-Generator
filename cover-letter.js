8a00a85473bdd52c2a69154fef7cfed9b61baaca
Witness327 <48520309+Witness327@users.noreply.github.com>
2022-01-27 20:35:12 -0500

    Update cover-letter.js
    Updated cover letter.

--- cover-letter.js
+++ cover-letter.js
@@ -10,7 +10,7 @@ var year = today.getFullYear()
 
 const coverLetter= 
 `<p> ANDRES PEREZ <br>
-Deerfield Beach, FL 33442 | (C) (561) 213-7707 | AndresPerez327@Gmail.com <br><br>
+Fort Pierce, FL 34951 | (C) (561) 213-7707 | AndresPerez327@Gmail.com <br><br>
 
 ${month} ${day}, ${year}<br>
 RE: ${role} <br><br>
@@ -19,12 +19,12 @@ I read with enthusiasm your recent job opportunity as a ${role} and I am writing
 
 What a thrill it would be to work for your team! I would love to speak with you regarding the position and how I could be a key player on your team. It would be an honor and a blessing to be a part of your company of leaders.<br><br>
 
-I bring a comprehensive set of skills that I believe will be valuable to your company. In my time as a student and a freelance web developer, I honed my abilities in both frontend and backend development, providing a firm foundation for the ${role} position. I am excited to contribute my talents and strengths to your teams efforts.<br><br>
+I bring a comprehensive set of skills that I believe will be valuable to your company. In my time as a .NET Developer, I honed my abilities in both frontend and backend development, providing a firm foundation for the ${role} position. I am excited to contribute my talents and strengths to your teams efforts.<br><br>
 
 Please review my enclosed resume for a more in-depth illustration of my work history and accomplishments. I would appreciate the opportunity to interview at your earliest convenience. I’m eager to discuss how my personality and background fit the ${role} role.<br><br>
 
 Please feel free to take a look at my project page below: <br>
-<a href="https://witness327.github.io/Project-Page/index.html" target="_blank"><strong>Witness327.GitHub.io/Project-Page</strong></a> <br><br>
+<a href="https://witness327.github.io/Signup-Form/" target="_blank"><strong>Witness327.GitHub.io/Signup-Form</strong></a> <br><br>
 
 Thank you for your time and consideration of my candidacy<br>
 Sincerely, Andres Perez </p>`
