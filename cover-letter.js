bc9a222934d202342b2a6e951a660ce02535643b
Witness327 <48520309+Witness327@users.noreply.github.com>
2024-01-09 09:16:35 -0500

    added additional carriage return. Updated projects section.

--- cover-letter.js
+++ cover-letter.js
@@ -12,7 +12,7 @@ const coverLetter=
 `<p> ANDRES PEREZ <br>
 Fort Pierce, FL 34951 | (C) (561) 213-7707 | AndresPerez327@Gmail.com <br><br>
 
-${month} ${day}, ${year}<br>
+${month} ${day}, ${year},<br>
 RE: ${role} <br><br>
 Dear ${companyName}, <br>
 I read your recent job opportunity as a ${role} and I am writing to express my deepest interest. When reviewing the position requirements and your organization’s website, I was excited to find that my qualifications and personal strengths align with your needs and mission.<br><br>
@@ -24,9 +24,12 @@ I bring a comprehensive set of skills that will be valuable to your company. As 
 Please review my enclosed resume for a more in-depth illustration of my work history and accomplishments. I would appreciate the opportunity to interview at your earliest convenience. I’m eager to discuss how my personality and background fit the ${role} role.<br><br>
 
 Please feel free to take a look at the projects below: <br>
-<a href="https://witness327.github.io/Signup-Form/" target="_blank"><strong>witness327.GitHub.io/Signup-Form</strong></a><br>
+-Our app was featured on CBS News Chicago, which showcased St. Anthony's Hospital using it in the Mother Baby Unit:<br>
+<a href="https://www.cbsnews.com/chicago/news/hospital-technology-loved-ones-maternity-rooms-afar/" target="_blank"><strong>https://www.cbsnews.com/chicago/news/hospital-technology-loved-ones-maternity-rooms-afar/</strong></a><br><br>
+-iOS App Store<br>
 <a href="https://apps.apple.com/us/app/journeypx/id1671022441" target="_blank"><strong>https://apps.apple.com/us/app/journeypx/id1671022441</strong></a><br>
-<a href="https://play.google.com/store/apps/details?id=com.journeypx.connect&hl=en" target="_blank"><strong>https://play.google.com/store/apps/details?id=com.journeypx.connect&hl=en</strong></a><br>
+-Google Play Store<br>
+<a href="https://play.google.com/store/apps/details?id=com.journeypx.connect&hl=en" target="_blank"><strong>https://play.google.com/store/apps/details?id=com.journeypx.connect&hl=en</strong></a><br><br>
 
 If you wish to check out additional projects, please click on the link below: <br>
 <a href="https://witness327.github.io/Project-Page/projects.html" target="_blank"><strong>witness327.github.io/Project-Page/projects.html</strong></a><br><br>
